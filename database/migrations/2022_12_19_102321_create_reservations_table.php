77cde0d1bbfe189d06195900327841ec6b04b95a
apavlopoulos <apavlopoulos@singularlogic.eu>
2022-12-29 17:05:43 +0200

    Update 2022_12_19_102321_create_reservations_table.php

--- database/migrations/2022_12_19_102321_create_reservations_table.php
+++ database/migrations/2022_12_19_102321_create_reservations_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->bigInteger('venue_id')->unsigned()->index();
             $table->foreign('venue_id')->references('id')->on('venues')->onDelete('cascade');
             $table->string('username',255);
-            $table->integer('number_of_seats');
+            $table->integer('number_of_seats',min("1"));
             $table->string('email');
             $table->string('company');
             $table->string('phone_number');
