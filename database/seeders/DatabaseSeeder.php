b897c9c0f9f7653b683a8fc77b7562c56288f11e
gkalogerakos <gkalogerakos@singularlogic.eu>
2023-01-04 16:43:09 +0200

    seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,8 @@ class DatabaseSeeder extends Seeder
     {
 
         $this->call([
-            VenueSeeder::class]);
+            VenueSeeder::class
+        ]);
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
--- database/seeders/VenueSeeder.php
+++ database/seeders/VenueSeeder.php
@@ -16,10 +16,41 @@ class VenueSeeder extends Seeder
      */
     public function run()
     {
-        Venue::factory()
-        ->count(5)
-        ->create();
+        $venues = [
+            [
+                'capacity' => 300,
+                'venue_date' => '2023-01-29',
+                'venue_time' => '21:00:00',
+                'location' => 'Θέατρο Αλάμπρα',
+                'free_seats' => 300,
+                'status' => 'ACTIVE',
+            ], [
+                'capacity' => 300,
+                'venue_date' => '2023-01-30',
+                'venue_time' => '21:00:00',
+                'location' => 'Θέατρο Αλάμπρα',
+                'free_seats' => 300,
+                'status' => 'ACTIVE',
+            ], [
+                'capacity' => 300,
+                'venue_date' => '2023-02-02',
+                'venue_time' => '21:00:00',
+                'location' => 'Θέατρο Αλάμπρα',
+                'free_seats' => 300,
+                'status' => 'ACTIVE',
+            ],
+            [
+                'capacity' => 300,
+                'venue_date' => '2023-02-05',
+                'venue_time' => '21:00:00',
+                'location' => 'Θέατρο Αλάμπρα',
+                'free_seats' => 300,
+                'status' => 'ACTIVE',
+            ],
+        ];
 
-        //
+        foreach ($venues as $key => $value) {
+            Venue::create($value);
+        }
     }
 }
