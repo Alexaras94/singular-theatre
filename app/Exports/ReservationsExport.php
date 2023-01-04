1709d3109dff58ef1ed3adc6ec455fe9384039b3
gkalogerakos <gkalogerakos@singularlogic.eu>
2023-01-04 15:31:56 +0200

    excel 2.0

--- app/Exports/ReservationsExport.php
+++ app/Exports/ReservationsExport.php
@@ -3,7 +3,6 @@
 namespace App\Exports;
 
 use App\Models\Reservation;
-use App\Models\Venue;
 use Illuminate\Support\Facades\DB;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
@@ -14,7 +13,7 @@ use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 use PhpParser\Node\Expr\Cast\Object_;
 
 
-class ReservationsExport implements FromCollection,withHeadings,ShouldAutoSize,WithStyles
+class ReservationsExport implements FromCollection, withHeadings, ShouldAutoSize, WithStyles
 {
 
 
@@ -27,33 +26,29 @@ class ReservationsExport implements FromCollection,withHeadings,ShouldAutoSize,W
         $reservations = Reservation::select('venue_id', 'username', 'number_of_seats', 'company', 'email', 'phone_number')->orderBy('venue_id')->get();
 
         foreach ($reservations as $r) {
-
-            $r->venue_date = Venue::find($r->venue_id)->venue_date;
-
-
-
+            $r->venue_date = $r->venue->venue_date;
+            $r->venue_time = $r->venue->venue_time;
         }
 
-
         return $reservations;
-
     }
 
 
 
-   
+
 
 
 
     public function headings(): array
     {
-        return ['Παράσταση','Όνομα','Αριθμός Θέσεων','Εταιρεία','Email','Αριθμός Τηλεφώνου','Ημερομηνία Παράστασης'];
+        return ['Παράσταση', 'Όνομα', 'Αριθμός Θέσεων', 'Εταιρεία', 'Email', 'Αριθμός Τηλεφώνου', 'Ημερομηνία Παράστασης', 'Ώρα Παράστασης'];
     }
 
     public function styles(Worksheet $sheet)
 
     {
         return [
-        1    => ['font' => ['bold' => true]]];
+            1    => ['font' => ['bold' => true]],
+        ];
     }
 }
