c0964f4e3335157322025a505118dac2e0804899
apavlopoulos <apavlopoulos@singularlogic.eu>
2023-01-20 18:10:43 +0200

    HotFixes

--- app/Exports/ReservationsExport.php
+++ app/Exports/ReservationsExport.php
@@ -6,28 +6,37 @@ use App\Models\Reservation;
 use Hamcrest\Core\AllOf;
 use Illuminate\Support\Facades\DB;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\HasReferencesToOtherSheets;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
 use Maatwebsite\Excel\Concerns\WithEvents;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\WithMapping;
+use Maatwebsite\Excel\Concerns\WithMultipleSheets;
 use Maatwebsite\Excel\Concerns\WithStyles;
 use Maatwebsite\Excel\Events\AfterSheet;
+use Maatwebsite\Excel\Events\BeforeExport;
 use Maatwebsite\Excel\Events\BeforeSheet;
+use Maatwebsite\Excel\Events\BeforeWriting;
+use Maatwebsite\Excel\Excel;
+use Maatwebsite\Excel\Files\LocalTemporaryFile;
 use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 use PhpParser\Node\Expr\Cast\Object_;
 
 
-class ReservationsExport implements FromCollection, withHeadings, ShouldAutoSize, WithStyles,WithMapping,WithEvents
+class ReservationsExport implements FromCollection, withHeadings, ShouldAutoSize, WithStyles,WithMapping,WithEvents,WithMultipleSheets
 {
     public function collection()
+
+
     {
-        return Reservation::with('venue')->get();
+
+        return Reservation::orderBy('venue_id','ASC')->with('venue')->get();
     }
 
 
     public function headings(): array
     {
-        return ['Παράσταση', 'Ημερομηνία Παράστασης', 'Ώρα Παράστασης', 'Όνομα', 'Αριθμός Θέσεων', 'Εταιρεία', 'Email', 'Αριθμός Τηλεφώνου'];
+        return ['Παράσταση', 'Ημερομηνία Παράστασης', 'Ώρα Παράστασης', 'Όνομα', 'Αριθμός Θέσεων', 'Εταιρεία', 'Email', 'Αριθμός Τηλεφώνου' ];
     }
 
     public function styles(Worksheet $sheet)
@@ -49,13 +58,29 @@ class ReservationsExport implements FromCollection, withHeadings, ShouldAutoSize
             $reservation->number_of_seats,
             $reservation->company,
             $reservation->email,
-            $reservation->phone_number
+            $reservation->phone_number,
+
+
+
+
+
         ];
     }
 
     public function registerEvents(): array
     {
         return [
+
+
+            BeforeExport::class=>function(BeforeExport $event){
+
+              //  $event->writer->reopen(new \Maatwebsite\Excel\Files\LocalTemporaryFile(storage_path('app/exports/ReservationsExport.xls')),Excel::XLS);
+
+
+              //  $event->writer->getSheetByIndex(0)->setCellValue('K3','VALUE ADDED');
+            },
+
+
             AfterSheet::class    => function(AfterSheet $event) {
                 $event->sheet->freezePane('A2');
 
@@ -65,5 +90,30 @@ class ReservationsExport implements FromCollection, withHeadings, ShouldAutoSize
                    ->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER);
         }
         ];
+
+
+
+
+
+    }
+
+
+
+
+
+    public function sheets(): array
+    {
+
+        $sheets =[];
+        $sheets[]=new ReservationsExport();
+        $sheets[]=new StatsExport();
+
+
+        return $sheets;
+
     }
 }
+
+
+
+
---
+++ app/Exports/StatsExport.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Exports;
+
+
+
+use App\Models\Reservation;
+use App\Models\Venue;
+use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\FromQuery;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithCustomValueBinder;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithMapping;
+use Maatwebsite\Excel\Concerns\WithStyles;
+use Maatwebsite\Excel\DefaultValueBinder;
+use Maatwebsite\Excel\Sheet;
+use mysql_xdevapi\Collection;
+use PhpOffice\PhpSpreadsheet\Cell\Cell;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
+
+class StatsExport implements  FromCollection , WithMapping ,WithHeadings ,ShouldAutoSize,WithStyles
+{
+
+
+
+    public function Collection()
+    {
+
+        $venues=Venue::all();
+        foreach ($venues as $v){
+            $v->sum=Reservation::where('venue_id',$v->id)->sum('number_of_seats');
+        }
+
+        $totalsum=new Venue();
+
+
+        $totalsum->venue_date="ΣΥΝΟΛΟ";
+        $totalsum->sum=Reservation::sum('number_of_seats');
+        $venues->push($totalsum);
+
+
+        $companies=Reservation::select('company')->distinct()->get();
+
+        info($companies);
+
+
+        $space=new Venue();
+
+        $venues->push($space);
+        $venues->push($space);
+        $venues->push($space);
+        $venues->push($space);
+
+
+        foreach ($companies as $c){
+            $company=new Venue;
+            $company->venue_date=$c->company;
+            $company->sum=Reservation::where('company',$c->company)->sum('number_of_seats');
+            $venues->push($company);
+        }
+
+
+        $totalsum=new Venue();
+
+        $totalsum->venue_date="ΣΥΝΟΛΟ";
+        $totalsum->sum=Reservation::sum('number_of_seats');
+        $venues->push($totalsum);
+
+
+        return $venues;
+
+
+    }
+
+
+
+
+    public function headings(): array
+    {
+        return [ 'Ημερομηνία Παράστασης', 'Άθροισμα Θέσεων'];
+
+    }
+
+    public function map($venue): array
+    {
+
+        return [
+            $venue->venue_date,
+            $venue->sum
+
+        ];
+    }
+
+    public function styles(Worksheet $sheet)
+    {
+        return
+       [ 'A'  => ['font' => ['bold' => true]] , '1'  => ['font' => ['bold' => true] ]
+
+       ];
+    }
+}
