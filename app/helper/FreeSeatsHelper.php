d2b629c2d9c009768b2a45f25eb7fb8d3115ce0f
gkalogerakos <gkalogerakos@singularlogic.eu>
2023-01-30 10:37:40 +0200

    corored dates fix

--- app/helper/FreeSeatsHelper.php
+++ app/helper/FreeSeatsHelper.php
@@ -9,7 +9,7 @@ class FreeSeatsHelper
     {
         if ($venue->free_seats >= (0.6 * $venue->capacity)) {
             return "#32cd32";
-        } elseif ($venue->free_seats >= (0.1 * $venue->capacity)) {
+        } elseif ($venue->free_seats >= (0.2 * $venue->capacity)) {
             return "#FF8A33";
         } else {
             return "#FF3333";
