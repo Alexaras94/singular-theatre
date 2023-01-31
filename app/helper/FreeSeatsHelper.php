75fe7a503cb88f7dc90ee700a83ed812d456b2f3
gkalogerakos <gkalogerakos@singularlogic.eu>
2023-01-31 11:19:51 +0200

    date color fix

--- app/helper/FreeSeatsHelper.php
+++ app/helper/FreeSeatsHelper.php
@@ -7,9 +7,9 @@ class FreeSeatsHelper
 
     public function DropDownColour($venue)
     {
-        if ($venue->free_seats >= (0.6 * $venue->capacity)) {
+        if ($venue->free_seats >= (0.2 * $venue->capacity)) {
             return "#32cd32";
-        } elseif ($venue->free_seats >= (0.2 * $venue->capacity)) {
+        } elseif ($venue->free_seats >= (0.1 * $venue->capacity)) {
             return "#FF8A33";
         } else {
             return "#FF3333";
